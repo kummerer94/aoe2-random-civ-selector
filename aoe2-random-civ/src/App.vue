de3ae178ef2ba042e8b17f34ecb6a33bc606075f
Alexander Kammerer <alexander.kammerer@online.de>
2020-04-09 00:46:01 +0200

    First preparations for application (build simple list)

--- aoe2-random-civ/src/App.vue
+++ aoe2-random-civ/src/App.vue
@@ -1,60 +1,92 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Random Selector</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>
+        AoE II - Definitive Edition - Random Civilization
+        Selector
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <ul>
+              <v-row dense v-for="civ in civilizations" :key="civ.name" justify="space-around">
+                <v-col>
+                  <v-switch
+                    :style="civ.isExcluded ? 'text-decoration: line-through' : ''"
+                    v-model="civ.isExcluded"
+                    :label="civ.name"
+                    dense
+                  >
+                    <template v-slot:append>
+                      <v-btn
+                        v-if="!civ.wasSelectedBefore"
+                        dense
+                        class="mx-5"
+                        style="text-decoration: none !important"
+                        @click="civ.wasSelectedBefore = !civ.wasSelectedBefore"
+                      >&times;</v-btn>
+                      <v-btn
+                        class="mx-5"
+                        style="text-decoration: none !important"
+                        v-else
+                        dense
+                        @click="civ.wasSelectedBefore = !civ.wasSelectedBefore"
+                      >+</v-btn>
+                      <v-chip>{{ civ.dlc }}</v-chip>
+                    </template>
+                  </v-switch>
+                </v-col>
+              </v-row>
+            </ul>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import civilizations from "./civ";
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  props: {
+    source: String
   },
-
   data: () => ({
-    //
+    drawer: null,
+    civilizations: civilizations
   }),
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
 };
 </script>
